a0fe2f21a9e7a961c4f7652e46783fc31f400e21
Shao Tan <t.shaoming@gmail.com>
2022-07-03 14:52:54 +0800

    Define base logic to display english and french words in canvas as well as identify box that user clicked on

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import Canvas from './components/Canvas'
 
 function App() {
-  return <Canvas />
+  const wordsMapping = {
+    "forest":	"forêt",
+    "sibling":	"frère et sœur",
+    "cereal":	"céréale",
+    "desk":	"bureau",
+    "camel":	"chameau",
+    "butter":	"beurre",
+    "bicycle":	"vélo",
+    "railroad":	"chemin de fer",
+    "folder":	"dossier",
+    "weekly":	"hebdomadaire",
+    "hungry":	"faim",
+    "limestone":	"calcaire",
+  }
+  return <Canvas wordsMapping={wordsMapping}/>
 }
 
 export default App;
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,7 +1,15 @@
 import React, { useRef, useEffect, useState } from 'react'
 import './Canvas.css';
 
+const WORD_HEIGHT = 50
+const BOX_WIDTH = 200
+
+type WordMapping = {
+  [name: string]: null | string
+}
+
 type CanvasProps = {
+  wordsMapping: WordMapping
 }
 
 type Position = {
@@ -16,8 +24,20 @@ type Line = {
   endY: number
 }
 
+enum WordType {
+  ENGLISH= 'english',
+  FRENCH= 'french'
+}
+
+type HasClickedBoxResult = false | {
+  clickedBox: boolean
+  xPosition: number
+  yPosition: number
+  wordType: WordType
+  wordIndex: number
+}
 const Canvas = (props:CanvasProps) => {
-  const { ...rest } = props
+  const { wordsMapping, ...rest } = props
   const canvasRef = useRef<HTMLCanvasElement>(null)
 
   // lineStart is used to track the start position of the current line
@@ -44,6 +64,85 @@ const Canvas = (props:CanvasProps) => {
     _setLines(line);
   }
 
+  const canvasHeight = Object.keys(wordsMapping).length * WORD_HEIGHT
+
+  // Check if mouse click is within a box region and returns the identifier of the box
+  const hasClickedBox = (x:number, y:number):HasClickedBoxResult => {
+    const canvas = canvasRef.current
+    if (!canvas) return false
+
+    const ctx = canvas.getContext('2d')
+    if (!ctx) return false
+    
+    let xPosition
+    let yPosition
+    let wordType
+    let wordIndex
+    let clickedBox
+    // Check if a box has been clicked and identify it
+    if (x <= BOX_WIDTH) {
+      xPosition = BOX_WIDTH
+      wordType = WordType.ENGLISH
+    } else if (x >= ctx.canvas.width - BOX_WIDTH) {
+      xPosition = ctx.canvas.width - BOX_WIDTH
+      wordType = WordType.FRENCH
+    } else {
+      return false
+    }
+
+    wordIndex = Math.floor(y/WORD_HEIGHT)
+    yPosition = wordIndex*WORD_HEIGHT+WORD_HEIGHT/2
+    clickedBox = true
+    return {
+      clickedBox,
+      xPosition,
+      yPosition,
+      wordType,
+      wordIndex,
+    }
+  }
+
+  const drawWordBoxes = () => {
+    const canvas = canvasRef.current
+    if (!canvas) return
+
+    const ctx = canvas.getContext('2d')
+    if (!ctx) return
+   
+    // Draw left side vertical lines
+    ctx.moveTo(BOX_WIDTH,0)
+    ctx.lineTo(BOX_WIDTH,ctx.canvas.height)
+    ctx.stroke()
+
+    // Draw right side vertical lines
+    ctx.moveTo(ctx.canvas.width-BOX_WIDTH,0)
+    ctx.lineTo(ctx.canvas.width-BOX_WIDTH,ctx.canvas.height)
+    ctx.stroke()
+
+    for(let i=0; i<Object.keys(wordsMapping).length; i++){
+      const englishWord = Object.keys(wordsMapping)[i]
+      const frenchWord = wordsMapping[englishWord]
+
+      // Draw left side horizontal lines
+      const yPosition = WORD_HEIGHT*(i+1)
+      ctx.moveTo(0,yPosition)
+      ctx.lineTo(BOX_WIDTH,yPosition)
+      ctx.stroke()
+      // Set english word
+      ctx.fillText(englishWord, BOX_WIDTH/2, yPosition-WORD_HEIGHT/2);
+
+      // Draw right side horizontal lines
+      ctx.moveTo(ctx.canvas.width-BOX_WIDTH,yPosition)
+      ctx.lineTo(ctx.canvas.width,yPosition)
+      ctx.stroke()
+      // Set french word
+      if (!frenchWord) {
+        throw new Error("French word missing")
+      }
+      ctx.fillText(frenchWord, ctx.canvas.width-BOX_WIDTH/2, yPosition-WORD_HEIGHT/2);
+    }
+  }
+
   // Active line is the current line where user is drawing and has yet to set the line end.
   // This function draws an active line on the canvas to give the user an idea of how the line looks like.
   const drawActiveLine = (x:number, y:number) => {
@@ -56,6 +155,8 @@ const Canvas = (props:CanvasProps) => {
   const handleClick = (x:number, y:number) => {
     const lineStart = lineStartRef.current
 
+    const result = hasClickedBox(x, y)
+    console.log("hasClickedBox", result)
     // This handles the case when a user just started drawing a line. Store the line start position
     if (!lineStart) {
       setLineStart({x,y})
@@ -90,6 +191,7 @@ const Canvas = (props:CanvasProps) => {
     setLines([])
     setLineStart(null)
     setLineEnd(null)
+    drawLines()
   }
 
   const drawLines = () => {
@@ -120,6 +222,7 @@ const Canvas = (props:CanvasProps) => {
       ctx.lineTo(lineEnd.x,lineEnd.y)
       ctx.stroke()
     }
+    drawWordBoxes()
   }
   
   useEffect(() => {
@@ -138,10 +241,11 @@ const Canvas = (props:CanvasProps) => {
       canvas.addEventListener("mouseout", function (e) {
         reset()
       })
+      drawWordBoxes()
     }
   }, [])
   
-  return <canvas className="Canvas" ref={canvasRef} {...props}/>
+  return <canvas className="Canvas" width="1000" height={canvasHeight} ref={canvasRef} {...props}/>
 }
 
 export default Canvas
